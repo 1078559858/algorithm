8a1c60399b118f5d39d7998b7528650488336027
liwei <liwei>
2020-11-18 18:42:16 +0800

    1

--- js/实现/new.js
+++ js/实现/new.js
@@ -1,7 +1,36 @@
-function myNew() {
-  var constructor = [].shift.call(arguments)
-  var obj = Object.create({})
-  obj.__proto__ = constructor.prototype;
-  constructor.call(obj, arguments)
-  return obj
+
+Promise.resolve(1).then(res => {
+  console.log(res)
+}).then(res => {
+  console.log(res)
+}).then(res => {
+  console.log(res)
+})
+
+Promise.resolve(1).then(res => {
+  console.log(res)
+}).then(res => {
+  console.log('1' + res)
+}).then(res => {
+  console.log('12' + res)
+}).then(res => {
+  console.log('123' + res)
+})
+
+async function async1() {
+  console.log('async1');
+  // thrownewError('error!!!')
+  return 'async1 success'
 }
+async1().then(res => console.log(res))
+
+new Promise(resolve => {
+  console.log('promise2')
+  setTimeout(() => {
+    console.log('timer')
+  })
+}).then(res => {
+  console.log('1' + res)
+}).then(res => {
+  console.log('2' + res)
+})
