0d92aee03ac18f3f9d4c25a923224d61c5b6c8a0
liwei <liwei>
2020-12-04 21:55:41 +0800

    事件委托

--- js/js/add(1)(2).js
+++ js/js/add(1)(2).js
@@ -1,30 +1,31 @@
 // 原生JS实现add(1)(2)(3)(4)的调用方式
+// 也可以使用柯里化实现。
 
 // 方法1
 var add = function (m) {
 
-    var temp = function (n) {
-        return add(m + n);
-    }
+  var temp = function (n) {
+    return add(m + n);
+  }
 
-    temp.toString = function () {
-        return m;
-    }
+  temp.toString = function () {
+    return m;
+  }
 
-    return temp;
+  return temp;
 };
 
 // 方法2
 function add(x) {
-    var sum = x;
-    var tmp = function (y) {
-        sum = sum + y;
-        return tmp;
-    };
-    tmp.toString = function () {
-        return sum;
-    };
+  var sum = x;
+  var tmp = function (y) {
+    sum = sum + y;
     return tmp;
+  };
+  tmp.toString = function () {
+    return sum;
+  };
+  return tmp;
 }
 
 console.log(add(3)(4)(5)); // 12
